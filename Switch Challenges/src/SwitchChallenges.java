282e38a189d3e33c91f482798fe3cdc2df39627a
jackson.paul <jackson.paul@MU-C1-12.mullen.pvt.k12.co.us>
2017-08-30 14:54:16 -0600

    finished challenge 1, started challenge 2

--- Switch Challenges/src/SwitchChallenges.java
+++ Switch Challenges/src/SwitchChallenges.java
@@ -1,10 +1,93 @@
-
+import java.util.Scanner;
 public class SwitchChallenges
 	{
 
 		public static void main(String[] args)
 			{
+			challenge1();
+			challenge2();
+			}
 
+		private static void challenge1()
+			{
+			int randomNumber = (int)(Math.random()*20 + 1);
+			System.out.println("I have just created a random number, which is " + randomNumber + ".");
+			switch (randomNumber)
+				{
+					case 2:
+					case 4:
+					case 6:
+					case 10:
+					case 12:
+					case 14:
+					case 18:
+					case 20:
+							{
+							System.out.println("You win!");
+							break;
+							}
+					case 1:
+					case 3:
+					case 7:
+					case 9:
+					case 11:
+					case 13:
+					case 15:
+					case 17:
+					case 19:
+							{
+							System.out.println("You tie!");
+							break;
+							}
+					default:
+							{
+							System.out.println("You lose!");
+							break;
+							}
+				}
+			}
+
+		private static void challenge2()
+			{
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("Buenos dias, I am the Magic 8 Ball. Please ask me a yes or no question.");
+			String userQuestion = userInput.nextLine();
+			int randomNumber = (int)(Math.random()*8 + 1);
+			switch (randomNumber)
+				{
+					case 1:
+							{
+							
+							}
+					case 2:
+							{
+							
+							}
+					case 3:
+							{
+							
+							}
+					case 4:
+							{
+							
+							}
+					case 5:
+							{
+							
+							}
+					case 6:
+							{
+							
+							}
+					case 7:
+							{
+							
+							}
+					case 8:
+							{
+							
+							}
+				}
 			}
 
 	}
